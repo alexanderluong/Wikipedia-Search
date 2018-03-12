6c15b8fd72044cedf54f77661ce80f7dcc09fbd6
alexanderluong <alexander.luong@hotmail.com>
2018-03-11 22:18:08 -0400

    Add ajax request to script.js

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+var articleName;
+
 $(document).ready(function() {
     // initialize tootltips
+    articleName = "example";
     $("#randomButton").tooltip({ delay: { show: 100 } });
+    getWikipediaData();
+
+    function getWikipediaData() {
+        $.ajax({
+                type: "GET",
+                dataType: "jsonp",
+                data: {
+                    action: "opensearch",
+                    search: articleName,
+                    format: "json",
+                    limit: "10",
+                    utf8: "1",
+                },
+                url: "https://en.wikipedia.org/w/api.php",
+            })
+            .done(function(data) {
+                console.log(data);
+            });
+    }
 });
